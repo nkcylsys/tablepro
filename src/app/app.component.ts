6f9adda6213f39658efa09d74918d63067833f6f
NK <nirmala.koli@cylsys.com>
2022-06-29 10:10:47 +0530

    tablepro

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonService } from './common.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'ktnpro';
+  allUser: Object | any;
+  isEdit=false;
+  userObj ={
+    name:'',
+    description:'',
+    webReference:'',
+    id:''
+  }
+
+  constructor(private commonService:CommonService){}
+  ngOnInit(){
+    this.getLatestUser();
+  }
+
+  addUser(formObj: any){
+    console.log(formObj)
+    this.commonService.createUser(formObj).subscribe((response)=>{
+      this.getLatestUser();
+    })
+  }
+  getLatestUser(){
+    this.commonService.getAllUser().subscribe((response)=>{
+      this.allUser = response
+    })
+  }
+  editUser(user: any){
+    this.isEdit = true;
+    this.userObj = user;
+  }
+
+  deleteUser(user: any){
+    this.commonService.deleteUser(user).subscribe(()=>{
+      this.getLatestUser(); 
+    })
+  }
+  updateUser(){
+    this.isEdit = !this.isEdit;
+    this.commonService.updateUser(this.userObj).subscribe(()=>{
+      this.getLatestUser(); 
+    })
+  }
+}
